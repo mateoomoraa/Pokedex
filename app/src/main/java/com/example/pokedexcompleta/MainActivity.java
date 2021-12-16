44ef3cd78bbfe81901b3a474ab518ac8f946edd4
HP196\CES <informatica@escuelaces.com>
2021-12-16 12:17:21 +0100

    modificacion Juan

--- app/src/main/java/com/example/pokedexcompleta/MainActivity.java
+++ app/src/main/java/com/example/pokedexcompleta/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listadoPkm);
-
+        System.out.println(" hola mundoooo");
 
         new Thread(new Runnable() {
             @Override
